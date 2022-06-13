edaa5cf5a4c0b647c51554b0e882b63b6c901da8
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2022-06-13 06:58:59 +0200

    main form 0.1

---
+++ src/Snappetty.App/FrmMain.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Snappetty.App
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(0, 0);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(217, 46);
+            this.btnCapture.TabIndex = 0;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 667);
+            this.Name = "FrmMain";
+            this.Text = "Snappetty";
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Button btnCapture;
+
+        #endregion
+    }
+}
---
+++ src/Snappetty.App/FrmMain.cs
@@ -0,0 +1,10 @@
+namespace Snappetty.App
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
